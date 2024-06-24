66b60e607956e9f1a81c9265fa5c0b5bfd77dccd
Ofonime Francis <ofonimeusoro01@gmail.com>
2024-06-24 23:55:03 +0200

    create security group

--- main.tf
+++ main.tf
@@ -22,7 +22,47 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = "Allow anything in and out"
+
+  vpc_id = data.aws_vpc.default.id 
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type      = "ingres"
+  from_port = 80
+  to_port   = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type      = "ingres"
+  from_port = 443
+  to_port   = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type      = "egres"
+  from_port = 0
+  to_port   = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
